64d431717c9dd4ef5808cd7864bb05f54900a6de
bobthesiddharth <bobthesiddharth@gmail.com>
2026-05-12 19:24:21 +0530

    added spring initializer

---
+++ localplate-backend/localPlates-spring-app/src/main/java/com/localplates/LocalPlates/LocalPlatesApplication.java
@@ -0,0 +1,13 @@
+git package com.localplates.LocalPlates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalPlatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalPlatesApplication.class, args);
+	}
+
+}
